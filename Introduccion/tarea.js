e1e864aba978fbe463464a74557d6e4bf1a18579
Francisco Duarte <francisco.duarte@galileo.edu>
2018-02-10 14:08:55 -0600

    se agrega el archivo

---
+++ Introduccion/tarea.js
@@ -0,0 +1,21 @@
+var mes = 10;
+
+if(mes == 1 || mes == 2 || mes == 12)
+{
+	alert("Es invierno");
+}
+else if(mes >= 3 && mes <= 5)
+{
+	alert("Es primavera");
+}
+
+else if (mes >= 6 && mes <= 8)
+{
+	alert("Es verano");
+}
+else if(mes >= 9 && mes <= 11)
+{
+	alert("Es otoño");
+}
+
+alert("Carné: 17001004 \nNombre: Francisco José Duarte Osorio");
